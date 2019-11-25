3360de9714917ea786d346b204322bcc80ef95c1
ljeferson <lucasjeferson6000@gmail.com>
2019-11-25 17:19:15 -0300

    aula 6 incompleto - ínicio  do Firebase

--- eventos/src/view/login/index.js
+++ eventos/src/view/login/index.js
@@ -4,15 +4,28 @@ import './login.css'
 function Login(){
     return(
         <div className="login-content d-flex align-items-center">
-            <form className="form-signin mx-auto">
-            <h1 className="h3 mb-3 font-weight-normal text-white">Login</h1>
+                <form className="form-signin mx-auto">
+                <div className= "mb-4 text-center">
+                    <h1 className="h3 mb-3 font-weight-normal text-white font-weight-bold">Login</h1>
 
-            <input type="email" id="inputEmail" class="form-control my-2" placeholder="Email address" />
+                    <input type="email" id="inputEmail" class="form-control my-2" placeholder="Email address" />
 
-            <input type="password" id="inputPassword" class="form-control my-2" placeholder="Password"/>
+                    <input type="password" id="inputPassword" class="form-control my-2" placeholder="Password"/>
 
-            <button className="btn btn-lg btn-block btn-login" type="submit">Sign in</button>
-            <p className="mt-5 mb-3 text-muted text-center">© 2017-2019</p>
+                    <button className="btn btn-lg btn-block btn-login" type="submit">Sign in</button>
+
+                    <div className="msg-login text-white text-center my-5">
+                        <span><strong>WoW! </strong>Você está conectado! &#128526; </span>
+                        <br></br>
+                        <span><strong>Ops! </strong>Verifique senha ou usuário estão corretos! &#128546; </span>
+                    </div>
+
+                    <div className="opcoes-login mt-5 text-center">
+                        <a href="#" className="mx-2">Recuperar Senha</a>
+                        <span className="text-white">&#9733;</span>
+                        <a href="#" className="mx-2">Quero Cadastrar</a>
+                    </div>
+                </div>
             </form>
         </div>
     )
