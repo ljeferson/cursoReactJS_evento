5d42068f9de320400769841af7165a34fdf8a601
ljeferson <lucasjeferson6000@gmail.com>
2019-11-26 16:21:55 -0300

    aula 7 completa - tela de cadastro de user

--- eventos/src/view/login/index.js
+++ eventos/src/view/login/index.js
@@ -8,13 +8,14 @@ function Login(){
 
     const [email, setEmail] = useState();
     const [senha, setSenha] = useState();
+    const [msgTipo, setMsgTipo] = useState();
 
     function logar(){
         
         firebase.auth().signInWithEmailAndPassword(email, senha).then(resultado => {
-            alert("Usuário Logado!");
+            setMsgTipo('sucesso');
         }).catch(erro => {
-                alert('Deu erro!' + erro);
+            setMsgTipo('erro');
         });
 
     }
@@ -24,7 +25,7 @@ function Login(){
                 <form className="form-signin mx-auto">
                 <div className= "mb-4 text-center">
     <h1 className="h3 mb-3 font-weight-normal text-white font-weight-bold">Login</h1>
-                    <div>
+                    <div className= "mb-4 text-center">
 
 
                         <input onChange={(e) => setEmail(e.target.value)} type="email" id="inputEmail" class="form-control my-2" placeholder="Email address" />
@@ -33,9 +34,8 @@ function Login(){
                         <button onClick={logar} className="btn btn-lg btn-block btn-login" type="button">Logar</button>
 
                         <div className="msg-login text-white text-center my-5">
-                            <span><strong>WoW! </strong>Você está conectado! &#128526; </span>
-                            <br></br>
-                            <span><strong>Ops! </strong>Verifique senha ou usuário estão corretos! &#128546; </span>
+                            {msgTipo == 'sucesso' && <span><strong>WoW! </strong>Você está conectado! &#128526; </span>}
+                            {msgTipo == 'erro' && <span><strong>Ops! </strong>Verifique senha ou usuário estão corretos! &#128546; </span>}
                         </div>
 
                         <div className="opcoes-login mt-5 text-center">
